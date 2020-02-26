22e633781457155f805a562e4593ed77b9f5b47d
Domenico Manna <mannadomenico2849@gmail.com>
2020-02-25 20:48:44 -0500

    Add documentation

--- server/Domain/Services/BodyFatPercentageCalculator.cs
+++ server/Domain/Services/BodyFatPercentageCalculator.cs
@@ -5,19 +5,24 @@ namespace Domain.Services
 {
     public class BodyFatPercentageCalculator
     {
+        /// <summary>
+        /// Calculates the body fat percentage for the given <paramref name="appUser"/> based off of the given 
+        /// <paramref name="bodyMeasurement"/>. Note, the units are all expected to be in inches.
+        /// </summary>
+        /// <returns> The body fat percentage </returns>
         public static double CalculateBodyFatPercentage(AppUser appUser, BodyMeasurement bodyMeasurement)
         {
             if (appUser.Gender == GenderType.Male)
             {
                 double maleBodyFatPercentage = 86.01 * Math.Log10(bodyMeasurement.WaistCircumference - bodyMeasurement.NeckCircumference) -
                     (70.041 * Math.Log10(appUser.Height)) + 36.76;
-                    
+
                 return maleBodyFatPercentage;
             }
 
             // Calculation for female
-            double femaleBodyFatPercentage = 163.205 * Math.Log10(bodyMeasurement.WaistCircumference + bodyMeasurement.HipCircumference - 
-                bodyMeasurement.NeckCircumference ) - (97.684 * Math.Log10(appUser.Height)) - 78.387;;
+            double femaleBodyFatPercentage = 163.205 * Math.Log10(bodyMeasurement.WaistCircumference + bodyMeasurement.HipCircumference -
+                bodyMeasurement.NeckCircumference) - (97.684 * Math.Log10(appUser.Height)) - 78.387; ;
 
             return femaleBodyFatPercentage;
         }
