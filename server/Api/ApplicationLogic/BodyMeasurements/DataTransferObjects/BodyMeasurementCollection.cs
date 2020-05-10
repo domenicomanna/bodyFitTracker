42f07637ffcd6fe6305b6e19f73048967128f243
Domenico Manna <mannadomenico2849@gmail.com>
2020-05-09 21:34:10 -0400

    Refactor code

--- server/Api/ApplicationLogic/BodyMeasurements/DataTransferObjects/BodyMeasurementCollection.cs
+++ server/Api/ApplicationLogic/BodyMeasurements/DataTransferObjects/BodyMeasurementCollection.cs
@@ -6,45 +6,26 @@ namespace Api.ApplicationLogic.BodyMeasurements.DataTransferObjects
 {
     public class BodyMeasurementCollection
     {
-        [JsonIgnore]
-        public MeasurementSystem MeasurementSystem { get; private set; }
-
-        [JsonIgnore]
-        public GenderType GenderType { get; private set; }
-
         public string MeasurementSystemName { get; private set;}
         public string GenderTypeName { get; private set; }
-
-        public Measurement Length {
-            get {
-                if (MeasurementSystem == MeasurementSystem.Imperial) {
-                    return new Measurement("Inches", "in");
-                }
-                // MeasurementSystem must be metric
-                else return new Measurement("Centimeters", "cm");
-            }
-        }
-
-        public Measurement Weight {
-            get {
-                if (MeasurementSystem == MeasurementSystem.Imperial){
-                    return new Measurement("Pounds", "lb");
-                }
-                // MeasurementSystem must be metric
-                else return new Measurement("Kilograms", "kg");
-            }
-        }
-
-
+        public Measurement Length { get; private set; }
+        public Measurement Weight { get; private set; }
         public IEnumerable<BodyMeasurementDTO> BodyMeasurements { get; private set; }
 
         public BodyMeasurementCollection(MeasurementSystem measurementSystem, GenderType genderType, IEnumerable<BodyMeasurementDTO> bodyMeasurements)
         {
-            MeasurementSystem = measurementSystem;
-            GenderType = genderType;
             MeasurementSystemName = measurementSystem.ToString();
             GenderTypeName = genderType.ToString();
             BodyMeasurements = bodyMeasurements;
+            
+            if (measurementSystem == MeasurementSystem.Imperial){
+                Weight = new Measurement("Pounds", "lb");
+                Length = new Measurement("Inches", "in");
+            }
+            else{
+                Weight = new Measurement("Kilograms", "kg");
+                Length = new Measurement("Centimeters", "cm");
+            }
         }
     }
 }
