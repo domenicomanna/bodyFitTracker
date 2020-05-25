0a880e2f09d42ce70b7469f54bc8f983a6a4c1a4
Domenico Manna <mannadomenico2849@gmail.com>
2020-05-25 18:50:38 -0400

    Add token to api requests

--- clientApp/src/api/baseConfiguration.ts
+++ clientApp/src/api/baseConfiguration.ts
@@ -1,9 +1,13 @@
 import axios from 'axios';
+import tokenKey from '../constants';
+
 axios.defaults.baseURL = 'https://localhost:5001/api/';
+
+const token = localStorage.getItem(tokenKey);
 
 axios.interceptors.request.use(
   config => {
-    config.headers['header'] = '...';
+    config.headers['Authorization'] = `Bearer ${token}`
     return config;
   },
   error => {
