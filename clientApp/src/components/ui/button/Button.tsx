7bdf1cc7ebb9d9fcbbfc15375acb814b570d1c94
Domenico Manna <mannadomenico2849@gmail.com>
2020-06-17 20:55:00 -0400

    Add loading icon in button when submit is clicked

--- clientApp/src/components/ui/button/Button.tsx
+++ clientApp/src/components/ui/button/Button.tsx
@@ -1,17 +1,26 @@
 import React, { FunctionComponent, CSSProperties } from 'react';
+import BarLoader from 'react-spinners/BarLoader';
 import styles from './button.module.css';
 
 type Props = {
   type: 'button' | 'submit';
-  buttonType: 'primary'
+  buttonClass: 'primary';
   style?: CSSProperties;
   disabled?: boolean;
+  isSubmitting?: boolean;
 };
 
-const Button: FunctionComponent<Props> = ({ buttonType, children, ...rest }) => {
+const Button: FunctionComponent<Props> = ({ buttonClass, isSubmitting, style, children, ...rest }) => {
+  const styleToHideText: CSSProperties | undefined = isSubmitting ? { color: 'transparent' } : undefined;
+  const loader = isSubmitting ? (
+    <BarLoader css={'position: absolute'} color={'#fff'} loading={true} height={10} width={120} />
+  ) : null;
+  const allButtonStyles: CSSProperties | undefined = { ...style, ...styleToHideText };
+
   return (
-    <button className={`${styles.button} ${styles[buttonType]}`} {...rest}>
+    <button style={allButtonStyles} className={`${styles.button} ${styles[buttonClass]}`} {...rest}>
       {children}
+      {loader}
     </button>
   );
 };
