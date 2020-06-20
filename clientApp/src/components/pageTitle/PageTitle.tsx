99ba3c493ba807a51b462b6c0bea5d9b7040e61f
Domenico Manna <mannadomenico2849@gmail.com>
2020-06-19 22:05:48 -0400

    Fix duplicate folder issue

--- clientApp/src/components/pageTitle/PageTitle.tsx
+++
@@ -1,9 +0,0 @@
-import React, { FunctionComponent } from 'react'
-
-const PageTitle:FunctionComponent = ({children}) => {
-    return (
-        <h1 style={{textAlign: "center", marginTop: 0}}>{children}</h1>
-    )
-}
-
-export default PageTitle
