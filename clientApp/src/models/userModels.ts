fcda8f988c616de62a22032143f1e1784abf88a5
Domenico Manna <mannadomenico2849@gmail.com>
2020-07-18 19:45:25 -0400

    Remove unused userModel

--- clientApp/src/models/userModels.ts
+++ clientApp/src/models/userModels.ts
@@ -9,14 +9,6 @@ export type MeasurementPreferenceModel = {
   weightUnit: string;
 };
 
-export type UserModel = {
-  token: string;
-  gender: Gender;
-  height: number;
-  measurementPreference: MeasurementPreferenceModel;
-  isAuthenticated: () => boolean;
-};
-
 export type UserSettings = {
   gender: Gender;
   height: number;
