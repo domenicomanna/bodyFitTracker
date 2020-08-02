a8772e6e5b1a69bcc639c67761922af7d7b8c43c
Domenico Manna <mannadomenico2849@gmail.com>
2020-08-02 13:20:10 -0400

    Add tests for password reset token validation

---
+++ server/ApiTests/ApplicationLogic/Users/Handlers/ValidateResetPasswordTokenHandlerTests.cs
@@ -0,0 +1,52 @@
+using System;
+using Api.ApplicationLogic.Users.DataTransferObjects;
+using Api.ApplicationLogic.Users.Handlers;
+using Api.Domain.Models;
+using Api.Persistence;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ApiTests.ApplicationLogic.Users.Handlers
+{
+    [TestClass]
+    public class ValidateResetPasswordTokenHandlerTests
+    {
+        ValidateResetPasswordTokenHandler _validateResetPasswordTokenHandler;
+        string _unexpiredToken = "unexpired";
+        string _expiredToken = "expired";
+
+        [TestInitialize]
+        public void SetUp()
+        {
+            BodyFitTrackerContext bodyFitTrackerContext = DatabaseConnectionFactory.GetInMemoryDatabase(true);
+
+            AppUser appUser = new AppUser("abc@gmail.com", "", "", 60, GenderType.Male, MeasurementSystem.Imperial);
+            bodyFitTrackerContext.PasswordResets.Add(new PasswordReset(_unexpiredToken, appUser.AppUserId, DateTime.Now.AddHours(10)));
+            bodyFitTrackerContext.PasswordResets.Add(new PasswordReset(_expiredToken, appUser.AppUserId, DateTime.Now.AddHours(-1)));
+            bodyFitTrackerContext.Add(appUser);
+            bodyFitTrackerContext.SaveChanges();
+
+            _validateResetPasswordTokenHandler = new ValidateResetPasswordTokenHandler(bodyFitTrackerContext);
+        }
+
+        [TestMethod]
+        public void ValidationShouldFailIfTokenIsNotFound()
+        {
+            ResetPasswordValidationResult result = _validateResetPasswordTokenHandler.Handle("random");
+            Assert.IsFalse(result.Succeeded);
+        }
+        
+        [TestMethod]
+        public void ValidationShouldFailIfTokenIsExipred()
+        {
+            ResetPasswordValidationResult result = _validateResetPasswordTokenHandler.Handle(_expiredToken);
+            Assert.IsFalse(result.Succeeded);
+        }
+
+        [TestMethod]
+        public void ValidationShouldPassIfTokenIsFoundAndNotExipred()
+        {
+            ResetPasswordValidationResult result = _validateResetPasswordTokenHandler.Handle(_unexpiredToken);
+            Assert.IsTrue(result.Succeeded);
+        }
+    }
+}
