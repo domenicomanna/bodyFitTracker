59fed77e6509be18cae6731e2c61fe27c7f64bc1
Domenico Manna <mannadomenico2849@gmail.com>
2020-08-04 16:44:43 -0400

    Add measurement conversion

---
+++ server/Api/Domain/Services/MeasurementConverter.cs
@@ -0,0 +1,24 @@
+using Api.Domain.Models;
+
+namespace Api.Domain.Services
+{
+    public class MeasurementConverter
+    {
+        static double _centimetersInOneInch = 2.54;
+        static double _kilogramsInOnePound = .45359237;
+
+        public static double ConvertLength(double value, MeasurementSystem source, MeasurementSystem destination)
+        {
+            if (source == destination) return value;
+            if (source == MeasurementSystem.Imperial) return value * _centimetersInOneInch; // convert to metric
+            return value / _centimetersInOneInch; // convert to imperial
+        }
+
+        public static double ConvertWeight(double value, MeasurementSystem source, MeasurementSystem destination)
+        {
+            if (source == destination) return value;
+            if (source == MeasurementSystem.Imperial) return value * _kilogramsInOnePound; // convert to metric
+            return value / _kilogramsInOnePound; // convert to imperial
+        }
+    }
+}
