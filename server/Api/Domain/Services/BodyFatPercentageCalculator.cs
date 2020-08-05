77de1f390d29faffe0d86b3a263381d408f18bad
Domenico Manna <mannadomenico2849@gmail.com>
2020-08-04 21:42:27 -0400

    Add ability to convert body measurements between different units

--- server/Api/Domain/Services/BodyFatPercentageCalculator.cs
+++ server/Api/Domain/Services/BodyFatPercentageCalculator.cs
@@ -7,14 +7,18 @@ namespace Api.Domain.Services
     {
         /// <summary>
         /// Calculates the body fat percentage based off of the given <paramref name="bodyMeasurement"/>. 
-        /// Note, the units are all expected to be in inches.
         /// </summary>
         /// <returns> The body fat percentage </returns>
         public static double CalculateBodyFatPercentage(BodyMeasurement bodyMeasurement)
         {
-            AppUser appUser = bodyMeasurement.AppUser;
+            BodyMeasurement bodyMeasurementConvertedToImperial = BodyMeasurementConverter.Convert(bodyMeasurement, bodyMeasurement.Units , MeasurementSystem.Imperial);
+            return CalculateBodyFatPercentageUsingImperialUnits(bodyMeasurementConvertedToImperial);
+        }
 
-            if (appUser.Gender == GenderType.Male)
+        private static double CalculateBodyFatPercentageUsingImperialUnits(BodyMeasurement bodyMeasurement)
+        {
+
+            if (bodyMeasurement.AppUser.Gender == GenderType.Male)
             {
                 double maleBodyFatPercentage = 86.01 * Math.Log10(bodyMeasurement.WaistCircumference - bodyMeasurement.NeckCircumference) -
                     (70.041 * Math.Log10(bodyMeasurement.Height)) + 36.76;
@@ -23,7 +27,7 @@ namespace Api.Domain.Services
             }
 
             // Calculation for female
-            double hipCircumference = (double) bodyMeasurement.HipCircumference;
+            double hipCircumference = (double)bodyMeasurement.HipCircumference;
             double femaleBodyFatPercentage = 163.205 * Math.Log10(bodyMeasurement.WaistCircumference + hipCircumference -
                 bodyMeasurement.NeckCircumference) - (97.684 * Math.Log10(bodyMeasurement.Height)) - 78.387; ;
 
---
+++ server/Api/Domain/Services/BodyMeasurementConverter.cs
@@ -0,0 +1,39 @@
+using System;
+using System.Collections.Generic;
+using Api.Domain.Models;
+
+namespace Api.Domain.Services
+{
+    public class BodyMeasurementConverter
+    {
+        public static List<BodyMeasurement> Convert(List<BodyMeasurement> bodyMeasurements, MeasurementSystem sourceUnits, MeasurementSystem destinationUnits)
+        {
+            if (sourceUnits == destinationUnits) return bodyMeasurements;
+            List<BodyMeasurement> convertedBodyMeasurements = new List<BodyMeasurement>();
+            foreach (BodyMeasurement bodyMeasurement in bodyMeasurements)
+            {
+                convertedBodyMeasurements.Add(Convert(bodyMeasurement, sourceUnits, destinationUnits));
+            }
+            return convertedBodyMeasurements;
+        }
+
+        public static BodyMeasurement Convert(BodyMeasurement bodyMeasurement, MeasurementSystem sourceUnits, MeasurementSystem destinationUnits)
+        {
+            if (sourceUnits == destinationUnits) return bodyMeasurement;
+            
+            double? hipCircumference = null;
+            double neckCircumference = MeasurementConverter.ConvertLength(bodyMeasurement.NeckCircumference, sourceUnits, destinationUnits);
+            double waistCircumference = MeasurementConverter.ConvertLength(bodyMeasurement.WaistCircumference, sourceUnits, destinationUnits);
+            if (bodyMeasurement.HipCircumference.HasValue)
+            {
+                hipCircumference = MeasurementConverter.ConvertLength((double) bodyMeasurement.HipCircumference, sourceUnits, destinationUnits);
+            }
+
+            double height = MeasurementConverter.ConvertLength(bodyMeasurement.Height, sourceUnits, destinationUnits);
+            double weight = MeasurementConverter.ConvertWeight(bodyMeasurement.Weight, sourceUnits, destinationUnits);
+
+            return new BodyMeasurement(bodyMeasurement.AppUser, neckCircumference, waistCircumference, hipCircumference, height, weight, bodyMeasurement.DateAdded, destinationUnits);
+        }
+
+    }
+}
