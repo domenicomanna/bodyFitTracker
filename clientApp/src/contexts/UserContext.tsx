a5ae145b41155c02d6919ce7358d0d1ddaa75b2d
Domenico Manna <mannadomenico2849@gmail.com>
2020-08-06 18:03:46 -0400

    Fetch user details in user context

--- clientApp/src/contexts/UserContext.tsx
+++ clientApp/src/contexts/UserContext.tsx
@@ -1,12 +1,14 @@
-import React, { createContext, useState, FunctionComponent } from 'react';
-import { MeasurementPreference, UserContextType, MeasurementSystemName } from '../types/userTypes';
+import React, { createContext, useState, FunctionComponent, useEffect } from 'react';
+import { MeasurementPreference, UserContextType, MeasurementSystemName, User } from '../types/userTypes';
 import { Gender } from '../types/userTypes';
 import tokenKey from '../constants/tokenKey';
 import isExpired from '../utils/tokenExpiration/tokenExpiration';
+import usersClient from '../api/usersClient';
 
 export const UserContext = createContext({} as UserContextType);
 
 const UserContextProvider: FunctionComponent = ({ children }) => {
+  const [userDetailsAreBeingFetched, setUserDetailsAreBeingFetched] = useState(true);
   const [gender, setGender] = useState<Gender>(Gender.Male);
   const [email, setEmail] = useState('');
   const [measurementPreference, setMeasurementPreference] = useState<MeasurementPreference>({
@@ -14,7 +16,7 @@ const UserContextProvider: FunctionComponent = ({ children }) => {
     weightUnit: 'lb',
     lengthUnit: 'in',
   });
-  const [height, setHeight] = useState(60);
+  const [height, setHeight] = useState(0);
 
   const isAuthenticated = (): boolean => {
     const token = localStorage.getItem(tokenKey);
@@ -22,9 +24,24 @@ const UserContextProvider: FunctionComponent = ({ children }) => {
     return !isExpired(token);
   };
 
+
+  useEffect(() => {
+    const fetchUser = async () => {
+      const user: User = await usersClient.getUser();
+      setHeight(user.height);
+      setEmail(user.email);
+      setGender(user.gender);
+      setMeasurementPreference(user.measurementPreference);
+      setUserDetailsAreBeingFetched(false)
+    };
+    if (isAuthenticated()) fetchUser();
+    else setUserDetailsAreBeingFetched(false);
+  }, []);
+
   return (
     <UserContext.Provider
       value={{
+        userDetailsAreBeingFetched,
         gender,
         height,
         email,
