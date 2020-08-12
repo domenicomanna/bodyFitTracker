82b3efd9d73ab27827038c97d80e8765f9023839
Domenico Manna <mannadomenico2849@gmail.com>
2020-08-12 19:12:18 -0400

    Round values

--- server/Api/ApplicationLogic/BodyMeasurements/DataTransferObjects/BodyMeasurementDTO.cs
+++ server/Api/ApplicationLogic/BodyMeasurements/DataTransferObjects/BodyMeasurementDTO.cs
@@ -4,30 +4,53 @@ namespace Api.ApplicationLogic.BodyMeasurements.DataTransferObjects
 {
     public class BodyMeasurementDTO
     {
+        private double _neckCircumference;
+        private double _waistCircumference;
+        private double? _hipCircumference;
+        private double _height;
+        private double _weight;
         private double _bodyFatPercentage;
 
         public int BodyMeasurementId { get; set; }
 
-        public double NeckCircumference { get; set; }
+        public double NeckCircumference
+        {
+            get { return Math.Round(_neckCircumference, 2); }
+            set { _neckCircumference = value; }
+        }
 
-        public double WaistCircumference { get; set; }
+        public double WaistCircumference
+        {
+            get { return Math.Round(_waistCircumference, 2); }
+            set { _waistCircumference = value; }
+        }
 
-        public double? HipCircumference { get; set; }
-
-        public double Height { get; set; }
-
-        public double Weight { get; set; }
-
-        public double BodyFatPercentage
+        public double? HipCircumference
         {
             get
             {
-                return Math.Round(_bodyFatPercentage, 2, MidpointRounding.AwayFromZero);
+                if (_hipCircumference == null) return null;
+                return Math.Round((double)_hipCircumference, 2);
             }
-            set
-            {
-                _bodyFatPercentage = value;
-            }
+            set { _hipCircumference = value; }
+        }
+
+        public double Height
+        {
+            get { return Math.Round(_height, 2); }
+            set { _height = value; }
+        }
+
+        public double Weight
+        {
+            get { return Math.Round(_weight, 2); }
+            set { _weight = value; }
+        }
+
+        public double BodyFatPercentage
+        {
+            get { return Math.Round(_bodyFatPercentage, 2); }
+            set { _bodyFatPercentage = value; }
         }
 
         public DateTime DateAdded { get; set; }
